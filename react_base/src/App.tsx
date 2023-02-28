50aa30f956b20c4b1350bb37826c83ce12475176
kazuya imoto <qu89ik66@gmail.com>
2023-02-28 13:13:56 +0900

    first commit

---
+++ react_base/src/App.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React, { useState } from "react";
+import "./App.css";
+import Hello from "./conponents/Hello";
+
+function App() {
+  console.log("Appコンポ最初");
+  const h1Style = {
+    color: "#FFF",
+    fontSize: "42px",
+    padding: "1em 2em",
+    backgroundImage: "linear-gradient(90deg, #c9ffbf 0%, #ffafbd 100%)",
+    marginBottom: "40px",
+    textShadow: "4px 4px 8px rgba(0,0,0,0.7)",
+  };
+  const mvStyle = {
+    minHeight: "100vh",
+    position: "relative" as "relative",
+  };
+  const mvImgStyle = {
+    width: "100vw",
+    height: "100vh",
+    objectFit: "cover" as "cover",
+    objectPosition: "center" as "center",
+  };
+  const mvTitleStyle = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    color: "#FFF",
+    fontSize: "48px",
+  };
+
+  const name = "React";
+  const user = {
+    firstName: "厚志",
+    lastName: "安堂",
+    height: 170,
+  };
+  const formatName = (user) => {
+    return user.firstName + " " + user.lastName;
+  };
+  const LinkPath = "https://apple.com";
+  const ImgPath = "https://picsum.photos/600/400";
+
+  const [num, setNum] = useState(0);
+  const countUp = () => {
+    setNum(num + 1);
+  };
+  const [mvFlag, setMvFlag] = useState(false);
+  const switchBtn = () => {
+    setMvFlag(!mvFlag);
+  };
+  const btnswitchStyle = {
+    position: "fixed" as "fixed",
+    left: "0",
+    zIndex: "100",
+  };
+  if (num % 3 === 0 && num > 0) {
+    //左側がfalseの場合、右側を処理　||
+    console.log("3の倍数");
+    mvFlag || setMvFlag(true);
+  } else {
+    //左側がtrueの場合、右側も処理 &&
+    console.log("Not3の倍数");
+    mvFlag && setMvFlag(false);
+  }
+  return (
+    <div className="App">
+      <button onClick={switchBtn} style={btnswitchStyle} id="btn__switch">
+        Toggle Switch
+      </button>
+      {mvFlag && (
+        <div style={mvStyle}>
+          {/* <a href={LinkPath}>Apple</a> */}
+          <img style={mvImgStyle} src={ImgPath} alt="" />
+          <h1 style={mvTitleStyle}>Hello World!</h1>
+        </div>
+      )}
+      <div>
+        <p style={{ fontSize: "40px" }}>{num}</p>
+        <button onClick={countUp}>カウントアップ</button>
+      </div>
+
+      <Hello name="Ando" color="blue" />
+      <Hello name="John Lennon" h1Style={h1Style}>
+        <br />
+        <span className="h1style__span">ポール</span>
+      </Hello>
+
+      <h1 style={h1Style}>Hello {name}</h1>
+      <h1>{formatName(user)}</h1>
+      <p>身長：{user.height + 3}</p>
+      <h1
+        style={{
+          color: "green",
+          marginBottom: "40px",
+          textShadow: "4px 4px 8px rgba(0,0,0,0.7)",
+        }}
+      >
+        Hello World!
+      </h1>
+      <p>Welcome to Cafe Du Ando</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_base/src/conponents/Hello.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = (props) => {
+  // console.log(props);
+  const { name, color, h1Style, children } = props;
+  return (
+    <h1 style={h1Style || { color: color }}>
+      {name}　{children}
+    </h1>
+  );
+};
+
+export default Hello;
