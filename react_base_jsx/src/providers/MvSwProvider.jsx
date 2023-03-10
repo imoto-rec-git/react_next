1cc91c3a0446fe641fa07cf072d2d57a08a3d75b
k.imoto <121611914+imoto-rec-git@users.noreply.github.com>
2023-03-10 15:34:54 +0900

    useContextについて

--- react_base_jsx/src/providers/MvSwProvider.jsx
+++ react_base_jsx/src/providers/MvSwProvider.jsx
@@ -1,7 +1,10 @@
 import React, { createContext } from 'react';
 import { useState } from 'react';
 
-export const MvSwContext = createContext();
+export const MvSwContext = createContext({
+  mvFlag: false,
+  setMvFlag: () => {},
+});
 export const MvSwProvider = (props) => {
   const { children } = props;
   const [mvFlag, setMvFlag] = useState(false);
